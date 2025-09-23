a5448eab76d88ce0f4fc049340a6367e4bece472
pox <iampoxel6@gmail.com>
2025-09-23 21:21:06 +0330

    refactor: remove commented lines

--- lua/nvtheme/themes/onedark.lua
+++ lua/nvtheme/themes/onedark.lua
@@ -37,76 +37,16 @@ M.colors = {
   pmenu_bg = "#61afef",
   folder_bg = "#61afef",
 }
--- M.colors = {
--- 	bg = "#282c34",
--- 	fg = "#bbc2cf",
---
--- 	bg_alt = "#21242b",
--- 	fg_alt = "#5b6268",
---
--- 	base0 = "#1b2229",
--- 	base1 = "#282c34",
--- 	base2 = "#32363e",
--- 	base3 = "#3c4048",
--- 	base4 = "#4e525a",
--- 	base5 = "#5a5e66",
--- 	base6 = "#a7aebb",
--- 	base7 = "#b3bac7",
--- 	base8 = "#bbc2cf",
---
--- 	red = "#ff6c6b",
--- 	orange = "#ea9558",
--- 	yellow = "#ECBE7B",
--- 	green = "#50a14f",
--- 	teal = "#98be65",
--- 	cyan = "#66c4ff",
--- 	magenta = "#a626a4",
--- 	violet = "#dc8ef3",
--- 	blue = "#48a6e6",
--- 	dark_red = "#c85a50",
--- 	grey = "#9ca0a4",
--- 	dark_blue = "#a0bcf8",
--- 	dark_cyan = "#005478",
---
--- }
--- M.light_colors = {
--- 	bg = "#fafafa",
--- 	fg = "#383a42",
---
--- 	bg_alt = "#f0f0f0",
--- 	fg_alt = "#c6c7c7",
---
--- 	base0 = "#bbc2cf",
--- 	base1 = "#b3bac7",
--- 	base2 = "#a7aebb",
--- 	base3 = "#5a5e66",
--- 	base4 = "#4e525a",
--- 	base5 = "#3c4048",
--- 	base6 = "#32363e",
--- 	base7 = "#282c34",
--- 	base8 = "#1b2229",
---
--- 	red = "#ff6c6b",
--- 	orange = "#ea9558",
--- 	yellow = "#ECBE7B",
--- 	green = "#50a14f",
--- 	teal = "#98be65",
--- 	cyan = "#66c4ff",
--- 	magenta = "#a626a4",
--- 	violet = "#dc8ef3",
--- 	blue = "#48a6e6",
--- 	dark_red = "#c85a50",
--- 	grey = "#9ca0a4",
--- 	dark_blue = "#a0bcf8",
--- 	dark_cyan = "#005478",
---
--- }
 
 M.polish_hl = {
 	treesitter = {
 		["@variable.member"] = { fg = M.colors.blue },
 		["@punctuation.bracket"] = { fg = M.colors.yellow },
 	},
+
+	default = {
+		Blink = { fg = M.colors.red }
+	}
 }
 
 M.type = "dark"
