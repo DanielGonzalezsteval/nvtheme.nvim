275a992040e7f1766814c4ab9623227dadd62f92
pox <iampoxel6@gmail.com>
2025-09-23 21:51:49 +0330

    feat(theme): add doom theme

---
+++ lua/nvtheme/themes/doom.lua
@@ -0,0 +1,48 @@
+-- Credits to nvchad https://github.com/nvchad/nvchad
+
+-- Credits to original https://github.com/NTBBloodbath/doom-one.nvim
+-- This is modified version of it
+
+local M = {}
+
+M.colors = {
+	white = "#bbc2cf",
+	darker_black = "#22262e",
+	black = "#282c34", --  nvim bg
+	black2 = "#2e323a",
+	one_bg = "#32363e", -- real bg of onedark
+	one_bg2 = "#3c4048",
+	one_bg3 = "#41454d",
+	grey = "#494d55",
+	grey_fg = "#53575f",
+	grey_fg2 = "#5d6169",
+	light_grey = "#676b73",
+	red = "#ff6b5a",
+	baby_pink = "#ff7665",
+	pink = "#ff75a0",
+	line = "#3b3f47", -- for lines like vertsplit
+	green = "#98be65",
+	vibrant_green = "#a9cf76",
+	nord_blue = "#47a5e5",
+	blue = "#51afef",
+	yellow = "#ECBE7B",
+	sun = "#f2c481",
+	purple = "#dc8ef3",
+	dark_purple = "#c678dd",
+	teal = "#4db5bd",
+	orange = "#ea9558",
+	cyan = "#46D9FF",
+	statusline_bg = "#2d3139",
+	lightbg = "#3a3e46",
+	pmenu_bg = "#98be65",
+	folder_bg = "#51afef",
+}
+
+M.polish_hl = {
+	["@variable.member"] = { fg = M.colors.blue },
+	["@punctuation.bracket"] = { fg = M.colors.yellow },
+}
+
+M.type = "dark"
+
+return M
